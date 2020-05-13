b404587a35c6e28d4dde2fec8014c4f71678361b
rafaelvinisan <Passe998021>
2020-05-13 18:23:53 -0300

    revisão

--- ConceitosIniciais/00InputOutput.py
+++ ConceitosIniciais/00InputOutput.py
@@ -1,9 +1,9 @@
-# o comando print escreve coisas na tela, por convenção o texto dentro do print fica entre aspas simples
+# o comando print escreve na tela, por convenção o texto dentro do print fica entre aspas simples
 # entretanto também funciona com aspas duplas
 print('hello World')
 print("ola Mundo")
 
-# O comando print, salta uma linha ao sem fim automaticamente, entretando podemos definir um "comando" alternativo para
+# O comando print, salta uma linha ao seu fim automaticamente, entretando podemos definir um "final alternativo" para
 # ser executado ao fim, utilizando o ", end='' ", onde o texto dentro das aspas simples será mostrado ao fim do comando
 # print ao inves de saltar linha
 
@@ -11,7 +11,7 @@ print('na mesma', end=' ')
 print('linha')
 
 # Para receber dados do usuário o comando utilizado é o "input()", este dado deve ser armazenado em alguma variavel criada
-# que não precisa necessáriamente receber um tipo. o comando input() pode receber uma string, essa será mostrada ao usuário
+# que não precisa necessáriamente receber um tipo. o comando input() pode receber uma string como parametro, essa será mostrada ao usuário
 # antes que ele insira o dado
 
 nome = input('digite um nome: ') # o nome digitado será armazenado na variavel "nome", note que a variavel nome não foi tipada, logo pode receber qualquer tipo de dado
@@ -25,7 +25,7 @@ print('você tem', idade, 'anos') # com a , podemos concatenar quaisquer tipos d
 # quando é preciso várias concatenações, o ideal é utilizar o método .format(), com ela utilizamos dentro da string o sinal
 # de {}, que será substituido ao final pelo dado desejado, inserido dentro do .format() em ordem de aparição
 
-print('ola {} você tem {} anos'.format(nome,idade))
+print('ola {} você tem {} anos'.format(nome, idade))
 
 # outra maneira de usar o print formatado é digitando um f antes de abrir as aspas simples. Isso permite que dentro das
 # chaves{} você já insira o dado que deseja que apareça naquela posição, eliminando a necessidade de usar o .format() ao final
